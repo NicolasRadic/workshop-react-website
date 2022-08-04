36d54b58d64894ea5d77244d314721a5ead289ec
NicolasRadic <nicolas.radic05@gmail.com>
2022-08-04 12:30:51 +0200

    add search function

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,14 +4,27 @@ import Card from "./Card";
 
 const Content = () => {
   const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState("");
+
   useEffect(() => {
     axios
-      .get(`http://localhost:3001/product`)
+      .get(`http://localhost:3001/product?q=${search}`)
       .then((res) => setProducts(res.data));
-  }, []);
+  }, [search]);
+
   return (
     <div className="content">
       <h2>Produits</h2>
+
+      <form>
+        <input
+          type="text"
+          placeholder="Entrez le nom d'un produit"
+          id="search-input"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+
       <div>
         {products.map((product) => (
           <Card key={product.id} product={product} />
